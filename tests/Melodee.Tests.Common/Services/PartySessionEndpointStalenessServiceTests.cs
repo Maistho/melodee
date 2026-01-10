3fe618ac29c94e9f940fff2911f991f4b1906c11
Steven Hildreth <steven@hixi.org>
2026-01-10 16:44:43 -0600

    feat: remove obsolete test files and update PartySessionEndpointStalenessServiceTests for clarity

--- tests/Melodee.Tests.Common/Services/PartySessionEndpointStalenessServiceTests.cs
+++ tests/Melodee.Tests.Common/Services/PartySessionEndpointStalenessServiceTests.cs
@@ -1,8 +1,9 @@
-using Melodee.Common.Configuration;
 using Melodee.Common.Data;
 using Melodee.Common.Data.Models;
+using Melodee.Common.Enums.PartyMode;
 using Melodee.Common.Services;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using Moq;
 using NodaTime;
 
@@ -13,20 +14,13 @@ namespace Melodee.Tests.Common.Services;
 /// </summary>
 public class PartySessionEndpointStalenessServiceTests : TestsBase
 {
-    private static IMelodeeConfiguration NewPartyModeConfiguration(int staleSeconds = 30)
-    {
-        var settings = NewConfiguration();
-        settings[PartyModeOptions.SectionName + ":" + nameof(PartyModeOptions.EndpointStaleSeconds)] = staleSeconds.ToString();
-        return new MelodeeConfiguration(settings);
-    }
-
     private static PartySessionEndpoint CreateEndpoint(Instant? lastSeenAt, int id = 1)
     {
         return new PartySessionEndpoint
         {
             Id = id,
             Name = "Test Endpoint",
-            Type = EndpointType.WebPlayer,
+            Type = PartySessionEndpointType.WebPlayer,
             ApiKey = Guid.NewGuid(),
             LastSeenAt = lastSeenAt,
             CreatedAt = SystemClock.Instance.GetCurrentInstant()
@@ -37,13 +31,9 @@ public class PartySessionEndpointStalenessServiceTests : TestsBase
     public void IsStale_ReturnsTrue_WhenLastSeenAtIsNull()
     {
         var loggerMock = new Mock<ILogger<PartySessionEndpointStalenessService>>();
-        var configFactoryMock = new Mock<IMelodeeConfigurationFactory>();
-        configFactoryMock.Setup(x => x.GetConfigurationAsync(It.IsAny<CancellationToken>()))
-            .ReturnsAsync(NewPartyModeConfiguration());
-
         var contextFactoryMock = new Mock<IDbContextFactory<MelodeeDbContext>>();
 
-        var service = new PartySessionEndpointStalenessService(loggerMock.Object, contextFactoryMock.Object, configFactoryMock.Object);
+        var service = new PartySessionEndpointStalenessService(loggerMock.Object, contextFactoryMock.Object);
         var endpoint = CreateEndpoint(null);
 
         Assert.True(service.IsStale(endpoint));
@@ -53,13 +43,9 @@ public class PartySessionEndpointStalenessServiceTests : TestsBase
     public void IsStale_ReturnsFalse_WhenLastSeenAtIsRecent()
     {
         var loggerMock = new Mock<ILogger<PartySessionEndpointStalenessService>>();
-        var configFactoryMock = new Mock<IMelodeeConfigurationFactory>();
-        configFactoryMock.Setup(x => x.GetConfigurationAsync(It.IsAny<CancellationToken>()))
-            .ReturnsAsync(NewPartyModeConfiguration());
-
         var contextFactoryMock = new Mock<IDbContextFactory<MelodeeDbContext>>();
 
-        var service = new PartySessionEndpointStalenessService(loggerMock.Object, contextFactoryMock.Object, configFactoryMock.Object);
+        var service = new PartySessionEndpointStalenessService(loggerMock.Object, contextFactoryMock.Object);
         var endpoint = CreateEndpoint(SystemClock.Instance.GetCurrentInstant());
 
         Assert.False(service.IsStale(endpoint));
@@ -69,13 +55,9 @@ public class PartySessionEndpointStalenessServiceTests : TestsBase
     public void IsStale_ReturnsTrue_WhenLastSeenAtIsOld()
     {
         var loggerMock = new Mock<ILogger<PartySessionEndpointStalenessService>>();
-        var configFactoryMock = new Mock<IMelodeeConfigurationFactory>();
-        configFactoryMock.Setup(x => x.GetConfigurationAsync(It.IsAny<CancellationToken>()))
-            .ReturnsAsync(NewPartyModeConfiguration());
-
         var contextFactoryMock = new Mock<IDbContextFactory<MelodeeDbContext>>();
 
-        var service = new PartySessionEndpointStalenessService(loggerMock.Object, contextFactoryMock.Object, configFactoryMock.Object);
+        var service = new PartySessionEndpointStalenessService(loggerMock.Object, contextFactoryMock.Object);
         var oldTimestamp = SystemClock.Instance.GetCurrentInstant() - Duration.FromSeconds(60);
         var endpoint = CreateEndpoint(oldTimestamp);
 
@@ -83,52 +65,28 @@ public class PartySessionEndpointStalenessServiceTests : TestsBase
     }
 
     [Fact]
-    public void IsStale_ReturnsFalse_WhenLastSeenAtIsExactlyAtThreshold()
+    public void IsStale_ReturnsTrue_WhenLastSeenAtIsAtOrBeforeThreshold()
     {
         var loggerMock = new Mock<ILogger<PartySessionEndpointStalenessService>>();
-        var configFactoryMock = new Mock<IMelodeeConfigurationFactory>();
-        configFactoryMock.Setup(x => x.GetConfigurationAsync(It.IsAny<CancellationToken>()))
-            .ReturnsAsync(NewPartyModeConfiguration());
-
         var contextFactoryMock = new Mock<IDbContextFactory<MelodeeDbContext>>();
 
-        var service = new PartySessionEndpointStalenessService(loggerMock.Object, contextFactoryMock.Object, configFactoryMock.Object);
-        var thresholdTimestamp = SystemClock.Instance.GetCurrentInstant() - TimeSpan.FromSeconds(30);
-        var endpoint = CreateEndpoint(thresholdTimestamp);
-
-        Assert.False(service.IsStale(endpoint));
-    }
-
-    [Fact]
-    public void IsStale_ReturnsTrue_WhenLastSeenAtIsOneSecondBeforeThreshold()
-    {
-        var loggerMock = new Mock<ILogger<PartySessionEndpointStalenessService>>();
-        var configFactoryMock = new Mock<IMelodeeConfigurationFactory>();
-        configFactoryMock.Setup(x => x.GetConfigurationAsync(It.IsAny<CancellationToken>()))
-            .ReturnsAsync(NewPartyModeConfiguration());
-
-        var contextFactoryMock = new Mock<IDbContextFactory<MelodeeDbContext>>();
-
-        var service = new PartySessionEndpointStalenessService(loggerMock.Object, contextFactoryMock.Object, configFactoryMock.Object);
-        var thresholdTimestamp = SystemClock.Instance.GetCurrentInstant() - TimeSpan.FromSeconds(31);
+        var service = new PartySessionEndpointStalenessService(loggerMock.Object, contextFactoryMock.Object);
+        var thresholdTimestamp = SystemClock.Instance.GetCurrentInstant() - Duration.FromSeconds(30);
         var endpoint = CreateEndpoint(thresholdTimestamp);
 
         Assert.True(service.IsStale(endpoint));
     }
 
     [Fact]
-    public void IsStale_UsesCustomStaleThreshold()
+    public void IsStale_ReturnsTrue_WhenLastSeenAtIsOneSecondBeforeThreshold()
     {
         var loggerMock = new Mock<ILogger<PartySessionEndpointStalenessService>>();
-        var configFactoryMock = new Mock<IMelodeeConfigurationFactory>();
-        configFactoryMock.Setup(x => x.GetConfigurationAsync(It.IsAny<CancellationToken>()))
-            .ReturnsAsync(NewPartyModeConfiguration(staleSeconds: 60));
-
         var contextFactoryMock = new Mock<IDbContextFactory<MelodeeDbContext>>();
 
-        var service = new PartySessionEndpointStalenessService(loggerMock.Object, contextFactoryMock.Object, configFactoryMock.Object);
-        var endpoint = CreateEndpoint(SystemClock.Instance.GetCurrentInstant() - Duration.FromSeconds(45));
+        var service = new PartySessionEndpointStalenessService(loggerMock.Object, contextFactoryMock.Object);
+        var thresholdTimestamp = SystemClock.Instance.GetCurrentInstant() - Duration.FromSeconds(31);
+        var endpoint = CreateEndpoint(thresholdTimestamp);
 
-        Assert.False(service.IsStale(endpoint));
+        Assert.True(service.IsStale(endpoint));
     }
 }
